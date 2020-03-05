813c0923c0d5cf66c19821da795745038002255f
Max van der Schee <m.vanderschee@tfe.nl>
2020-03-05 15:21:21 +0100

    setup default layout ✨

---
+++ components/base/Header.vue
@@ -0,0 +1,17 @@
+<template>
+	<header class="header">
+		<Logo />
+	</header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import { Logo } from '@/components/base';
+
+@Component({
+	components: {
+		Logo,
+	},
+})
+export default class Header extends Vue {}
+</script>
